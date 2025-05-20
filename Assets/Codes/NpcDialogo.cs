2296470c3221b6c9685f3c68056a369cc75590cf
Ferdando30 <99683197+Ferdando30@users.noreply.github.com>
2025-05-20 09:46:42 -0300

    Dialogo NPC

--- Assets/Codes/NpcDialogo.cs
+++ Assets/Codes/NpcDialogo.cs
@@ -1,13 +1,21 @@
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class NpcDialogo : MonoBehaviour
 {
     public WalkingCicleAI WalkingCicleAIScript;
+    public GameObject BGImage;
+    public TextMeshProUGUI NpcName;
+    public TextMeshProUGUI NpcText;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         WalkingCicleAIScript.moveSpeed = 6;
+        BGImage.SetActive(false);
+        NpcName.enabled = false;
+        NpcText.enabled = false;
     }
 
     // Update is called once per frame
@@ -16,6 +24,18 @@ public class NpcDialogo : MonoBehaviour
         if (Input.GetKey(KeyCode.E))
         {
             WalkingCicleAIScript.moveSpeed = 0;
+            BGImage.SetActive(true);
+            NpcName.enabled = true;
+            NpcText.enabled = true;
+        }
+
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            WalkingCicleAIScript.moveSpeed = 6;
+            BGImage.SetActive(false);
+            NpcName.enabled = false;
+            NpcText.enabled = false;
         }
     }
+    
 }
