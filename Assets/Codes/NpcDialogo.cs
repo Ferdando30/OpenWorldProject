08d36bbbd95633073c23e6855cb974179ecc4072
Ferdando30 <99683197+Ferdando30@users.noreply.github.com>
2025-05-20 16:25:21 -0300

    trigger do dialogo

--- Assets/Codes/NpcDialogo.cs
+++ Assets/Codes/NpcDialogo.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using System.Collections;
 
 public class NpcDialogo : MonoBehaviour
 {
@@ -8,34 +9,52 @@ public class NpcDialogo : MonoBehaviour
     public GameObject BGImage;
     public TextMeshProUGUI NpcName;
     public TextMeshProUGUI NpcText;
+    public TextMeshProUGUI UI;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        WalkingCicleAIScript.moveSpeed = 6;
+        WalkingCicleAIScript.moveSpeed = 5;
         BGImage.SetActive(false);
         NpcName.enabled = false;
         NpcText.enabled = false;
+        UI.enabled = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.E))
-        {
-            WalkingCicleAIScript.moveSpeed = 0;
-            BGImage.SetActive(true);
-            NpcName.enabled = true;
-            NpcText.enabled = true;
-        }
+        
 
         if (Input.GetKey(KeyCode.Escape))
         {
-            WalkingCicleAIScript.moveSpeed = 6;
+            WalkingCicleAIScript.moveSpeed = 5;
             BGImage.SetActive(false);
             NpcName.enabled = false;
             NpcText.enabled = false;
         }
     }
-    
+     void OnTriggerStay (Collider hit)
+    {
+        if(hit.tag == "Player")
+        {
+           UI.enabled = true;
+            if (Input.GetKey(KeyCode.E))
+            {
+                WalkingCicleAIScript.moveSpeed = 0;
+                BGImage.SetActive(true);
+                NpcName.enabled = true;
+                NpcText.enabled = true;
+            }
+        }
+    }
+    void OnTriggerExit (Collider hit)
+    {
+        if (hit.tag == "Player")
+        {
+            UI.enabled = false;
+            
+        }
+    }
+
 }
