a35f9503214e4d81a5816fe2b0eebe50c15c9925
Ferdando30 <99683197+Ferdando30@users.noreply.github.com>
2025-06-05 18:04:28 -0300

    dialogo
    dialogo do NPC da vila da pesca ta feito

--- Assets/Codes/NpcDialogo.cs
+++ Assets/Codes/NpcDialogo.cs
@@ -10,8 +10,17 @@ public class NpcDialogo : MonoBehaviour
     public WalkingCicleAI WalkingCicleAIScript;
     public GameObject BGImage;
     public TextMeshProUGUI NpcName;
-    public TextMeshProUGUI NpcText;
+    public TextMeshProUGUI NpcLine1;
+    public TextMeshProUGUI NpcLine2;
+    public TextMeshProUGUI NpcLine3;
+    public TextMeshProUGUI NpcLine4;
+    public TextMeshProUGUI NpcLine5;
+    public TextMeshProUGUI PressEsc;
     public TextMeshProUGUI UI;
+    public Button Ovo_Dourado;
+    public Button Proximo01;
+    public Button Proximo02;
+    public Button Proximo03;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -21,8 +30,18 @@ public class NpcDialogo : MonoBehaviour
         WalkingCicleAIScript.moveSpeed = 5;
         BGImage.SetActive(false);
         NpcName.enabled = false;
-        NpcText.enabled = false;
+        NpcLine1.enabled = false;
+        NpcLine2.enabled = false;
+        NpcLine3.enabled = false;
+        NpcLine4.enabled = false;
+        NpcLine5.enabled = false;
+        PressEsc.enabled = false;
         UI.enabled = false;
+        Cursor.visible = false;
+        Ovo_Dourado.gameObject.SetActive(false);
+        Proximo01.gameObject.SetActive(false);
+        Proximo02.gameObject.SetActive(false);
+        Proximo03.gameObject.SetActive(false);
     }
 
     // Update is called once per frame
@@ -35,7 +54,18 @@ public class NpcDialogo : MonoBehaviour
             WalkingCicleAIScript.moveSpeed = 5;
             BGImage.SetActive(false);
             NpcName.enabled = false;
-            NpcText.enabled = false;
+            NpcLine1.enabled = false;
+            NpcLine2.enabled = false;
+            NpcLine3.enabled = false;
+            NpcLine4.enabled = false;
+            NpcLine5.enabled = false;
+            PressEsc.enabled = false;
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+            Ovo_Dourado.gameObject.SetActive(false);
+            Proximo01.gameObject.SetActive(false);
+            Proximo02.gameObject.SetActive(false);
+            Proximo03.gameObject.SetActive(false);
         }
     }
      void OnTriggerStay (Collider hit)
@@ -50,7 +80,10 @@ public class NpcDialogo : MonoBehaviour
                 WalkingCicleAIScript.moveSpeed = 0;
                 BGImage.SetActive(true);
                 NpcName.enabled = true;
-                NpcText.enabled = true;
+                NpcLine1.enabled = true;
+                Cursor.lockState = CursorLockMode.None;
+                Cursor.visible = true;
+                Ovo_Dourado.gameObject.SetActive(true);
             }
         }
     }
@@ -63,6 +96,38 @@ public class NpcDialogo : MonoBehaviour
             UI.enabled = false;
             
         }
+    }
+
+   public void Button_One()
+    {
+        NpcLine1.enabled = false;
+        NpcLine2.enabled = true;
+        Ovo_Dourado.gameObject.SetActive(false);
+        Proximo01.gameObject.SetActive(true);
+    }
+
+    public void Button_Two()
+    {
+        NpcLine2.enabled = false;
+        NpcLine3.enabled = true;
+        Proximo01.gameObject.SetActive(false);
+        Proximo02.gameObject.SetActive(true);
+    }
+
+    public void Button_Three()
+    {
+        NpcLine3.enabled = false;
+        NpcLine4.enabled = true;
+        Proximo02.gameObject.SetActive(false);
+        Proximo03.gameObject.SetActive(true);
+    }
+
+    public void Button_Four()
+    {
+        NpcLine4.enabled = false;
+        NpcLine5.enabled = true;
+        Proximo03.gameObject.SetActive(false);
+        PressEsc.enabled = true;
     }
 
 }
