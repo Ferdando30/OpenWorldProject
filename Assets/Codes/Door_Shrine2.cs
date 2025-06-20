c6c7fd9b5ee2c081b373119be139bb89d9a35700
Ferdando30 <99683197+Ferdando30@users.noreply.github.com>
2025-06-20 16:55:39 -0300

    bugs
    nn consegui resolver o problema da shrine 2, ent agr só tem um inimigo nela

--- Assets/Codes/Door_Shrine2.cs
+++ Assets/Codes/Door_Shrine2.cs
@@ -5,10 +5,7 @@ public class Door_Shrine2 : MonoBehaviour
    //definindo variaveis dos objetos dos 5 inimigos e da porta
     
     public GameObject Enemy1;
-    public GameObject Enemy2;
-    public GameObject Enemy3;
-    public GameObject Enemy4;
-    public GameObject Enemy5;
+    
 
     public GameObject door;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -22,7 +19,7 @@ public class Door_Shrine2 : MonoBehaviour
     {
        //definindo que quando todos os inimigos forem mortos a porta ira abrir
         
-        if(Enemy1 == null && Enemy2 == null && Enemy3 == null && Enemy4 == null && Enemy5 == null)
+        if(Enemy1 == null)
         {
             door.SetActive(false);
         }
