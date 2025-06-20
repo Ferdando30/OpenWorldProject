b87be6d36a2db0bd41b1193710b9782fc04b4a9d
Ferdando30 <99683197+Ferdando30@users.noreply.github.com>
2025-06-20 16:13:37 -0300

    bugs
    coloquei o pause na boss room e resolvi o bug do caracol continuar spawnando dps de ser coletado

--- Assets/Codes/CaracolColetavel.cs
+++ Assets/Codes/CaracolColetavel.cs
@@ -7,7 +7,8 @@ public class CaracolColetavel : MonoBehaviour
 {
     public TextMeshProUGUI UI;
     public TextMeshProUGUI text;
-    
+    public string itemID = "caracol_001";
+
 
     public static int Caracois;
 
@@ -15,8 +16,13 @@ public class CaracolColetavel : MonoBehaviour
     void Start()
     {
         UI.enabled = false;
-       // Caracois = 0;
-        
+
+        if (PlayerPrefs.GetInt("Item_" + itemID, 0) == 1)
+        {
+            Destroy(gameObject); // Já foi coletado antes
+            return; // sair do Start
+        }
+
 
     }
 
@@ -33,7 +39,9 @@ public class CaracolColetavel : MonoBehaviour
             UI.enabled = true;
             if (Input.GetKey(KeyCode.E))
             {
-               
+                PlayerPrefs.SetInt("Item_" + itemID, 1);
+                PlayerPrefs.Save();
+
                 Caracois = Caracois + 1;
                 Destroy(gameObject);
                 UI.enabled = false;
